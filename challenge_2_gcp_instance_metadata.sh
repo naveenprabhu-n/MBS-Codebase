77a048756084b4782650dbfef27dc371b5faeeff
naveenprabhu-n <120745814+naveenprabhu-n@users.noreply.github.com>
2023-02-07 00:05:03 +0530

    Create challenge_2_gcp_instance_metadata.sh

---
+++ challenge_2_gcp_instance_metadata.sh
@@ -0,0 +1,10 @@
+#gcloud command is an easy way to get the details of any GCP service
+
+#The below command gives the metadata of all details associated with the VM in a "json" format. 
+gcloud compute instances describe $vm_instance_name --format="json" --zone=us-east4-a 
+
+
+#BONUS : To retrieve value for a particular data key individually, we can tweak, the describe command to fetch the output in "value" format, 
+#Replace $vm_instance_name with name of the instance and $key with the specific data key you want to fetch the value for.
+
+gcloud compute instances describe $vm_instance_name --format="value(metadata.items.$key.value)" --zone=us-east4-a 
